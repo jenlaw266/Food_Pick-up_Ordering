342ff39b1b4046c3d0d21dba2a227eda1cfe0123
Khushbu <khushbu.thakkar@outlook.com>
2021-09-18 20:01:59 +0000

    Add routes for customers and owner

--- server.js
+++ server.js
@@ -33,22 +33,25 @@ app.use(express.static("public"));
 
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
-const usersRoutes = require("./routes/users");
+const customersRoutes = require("./routes/customers");
+const ownerRoutes = require("./routes/owner");
 const widgetsRoutes = require("./routes/widgets");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
-app.use("/api/users", usersRoutes(db));
-app.use("/api/widgets", widgetsRoutes(db));
+//app.use("/api/customers", customersRoutes(db));
+app.use("/", customersRoutes(db));
+app.use("/owner", ownerRoutes(db));
+//app.use("/api/widgets", widgetsRoutes(db));
 // Note: mount other resources here, using the same pattern above
 
 
 // Home page
 // Warning: avoid creating more routes in this file!
 // Separate them into separate routes files (see above).
-app.get("/", (req, res) => {
-  res.render("index");
-});
+// app.get("/", (req, res) => {
+//   res.render("index");
+// });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
