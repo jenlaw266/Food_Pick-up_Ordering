e467746ee5807041cfcf2a488649466c8a11fc83
Ysabel G <87351768+ysabelgarcia10@users.noreply.github.com>
2021-09-18 16:30:04 -0600

    Delete db directory

--- db/schema/01_orders.sql
+++
@@ -1,9 +0,0 @@
--- Drop and recreate Users table (Example)
-
-DROP TABLE IF EXISTS orders CASCADE;
-CREATE TABLE orders (
-  id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255) NOT NULL,
-  phone VARCHAR(255) NOT NULL,
-  archived BOOLEAN NOT NULL DEFAULT FALSE
-);
--- db/seeds/02_dishes.sql
+++
@@ -1,7 +0,0 @@
--- Users table seeds here (Example)
-INSERT INTO dishes (item, description, price, estimated_time)
-VALUES
-('Carne Asada Fries', 'house cut fries topped with a carne asada steak, guacamole, pico de gallo, cilantro and drizzled with queso and sour cream', 14.75, 15),
-('Bulgogi Steak Tacos (3)', 'korean marinated sirloin, spicy slaw, pickled cucumber, carrot and serrano peppers', 20.00, 20),
-('Baja Fish Burritos', 'blackened tilapia, rice, cabbage, black beans, corn, guacamole, jalapeno ranch', 19.50, 25),
-('Churro Bites', 'classic street food treat, with nutella dip', 7.25, 10);
