9122c591a0a06dbe99f85eeffbb38f565da71245
Khushbu <khushbu.thakkar@outlook.com>
2021-09-19 23:25:36 +0000

    Add templates and css

--- routes/admin.js
+++ routes/admin.js
@@ -8,29 +8,36 @@
 const express = require('express');
 const router  = express.Router();
 
+
 const adminRouter = (db) => {
 
   // GET admin dashboard
   router.get('/', (req, res) => {
-     db.query('select orders.id, orders.name, orders.phone, sum(line_items.subtotal) as subTotal from orders join line_items on line_items.order_id = orders.id group by orders.id;')
-    .then((response)=>{
-      res.json(response.rows);
-    })
-    .catch((err)=> console.log(err));
-  });
+  //   db.query('select orders.id, orders.name, orders.phone, sum(line_items.subtotal) as subTotal from orders join line_items on line_items.order_id = orders.id group by orders.id;')
+  //  .then((response)=>{
+  //    console.log(response.rows);
+  //  })
+  //  .catch((err)=> console.log(err));
+  res.render('admin_dashboard');
+ });
 
   // GET operation order details
   router.get('/:id', (req, res) => {
     db.query(`SELECT line_items.id, dishes.item, line_items.qty, line_items.subtotal FROM orders JOIN line_items ON line_items.order_id = orders.id JOIN dishes ON dishes.id = line_items.dish_id where orders.id = $1;`,[req.params.id])
     .then((response)=>{
-      res.json(response.rows);
+       //res.json(response.rows);
+      res.render("admin_order_details");
+      res.redirect("/admin");
     })
     .catch((err)=> console.log(err));
   });
 
   // POST Edit operation And send SMS
-  router.post('/:id', (req, res) => {
-    res.send("Admin Add ETA and implement TWILIO")
+  router.post('/', (req, res) => {
+    //query the max time from line_items
+    //include the text input from
+    res.send("Admin Add ETA and implement TWILIO");
+    res.render("admin_order_details");
   });
 
   // return the router
