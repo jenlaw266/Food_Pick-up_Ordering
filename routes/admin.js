953ad1d1ae2197e3523dd62f6bcfab09da611aaf
Khushbu <khushbu.thakkar@outlook.com>
2021-09-21 17:32:37 +0000

    update time format for orders and display date on top

--- routes/admin.js
+++ routes/admin.js
@@ -24,7 +24,7 @@ const adminRouter = (db) => {
 
   // GET admin dashboard
   router.get('/', (req, res) => {
-    console.log("<---------- GET ADMIN PAGE ----------->");
+  //  console.log("<---------- GET ADMIN PAGE ----------->");
      db.query('select orders.id, orders.name, orders.phone, sum(line_items.subtotal) as subTotal, orders.status, orders.order_datetime from orders join line_items on line_items.order_id = orders.id group by orders.id;')
     .then((response)=>{
       console.log(response.rows)
@@ -42,7 +42,7 @@ const adminRouter = (db) => {
 
   // GET operation order details
   router.get('/:id', (req, res) => {
-    console.log("<---------- GET INDIVIDUAL ORDER PAGE ----------->");
+  //  console.log("<---------- GET INDIVIDUAL ORDER PAGE ----------->");
     db.query(`SELECT line_items.id, dishes.item, dishes.price, line_items.qty, line_items.subtotal FROM orders JOIN line_items ON line_items.order_id = orders.id JOIN dishes ON dishes.id = line_items.dish_id where orders.id = $1;`,[req.params.id])
     .then((response)=>{
        //res.json(response.rows);
@@ -54,7 +54,7 @@ const adminRouter = (db) => {
        };
       //return db.query("select max(estimated_time) from dishes join line_items on line_items.dish_id = dishes.id where order_id = 1;")
       //console.log("anORDER:",anOrder);
-      console.log("1 ", templateVars);
+      //console.log("1 ", templateVars);
       //res.render("admin_order_details", templateVars);
     })
     .then(() => {
@@ -62,7 +62,7 @@ const adminRouter = (db) => {
     })
     .then((response)=>{
       templateVars['maxTime'] = response.rows[0].max;
-      console.log("2 ", templateVars);
+    //  console.log("2 ", templateVars);
       res.render("admin_order_details", templateVars);
     })
     .catch((err)=> console.log(err));
@@ -71,10 +71,10 @@ const adminRouter = (db) => {
 //  POST Edit operation And send SMS
   router.post('/:id', (req, res) => {
 
-
-    const totalTime = Number(req.body.maxTime);
+    const totalTime = Number(req.body.maxTime) + Number(req.body.addTime);
+    console.log("add time ",Number(req.body.addTime));
     console.log("EXTRA TIME", totalTime);
-    console.log("id", req.params.id);
+    //console.log("id", req.params.id);
     db.query(`UPDATE orders SET status = 'PROCESSED', order_datetime = CURRENT_TIMESTAMP +  ($1 * interval '1 minute') WHERE id = $2;`,[totalTime,req.params.id])
     .then(()=>{
 
@@ -84,8 +84,8 @@ const adminRouter = (db) => {
     })
     .then((response) =>{
       const ordersDb = response.rows;
-        smsToCustomer(ordersDb[0]["order_datetime"]);
-         console.log(response.rows)
+      //  smsToCustomer(ordersDb[0]["order_datetime"]);
+      //   console.log(response.rows)
         console.log("datetime ", ordersDb[0]["order_datetime"]);
     })
     .then(()=>{
