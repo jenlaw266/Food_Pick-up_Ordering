35bec99f1c0d394779394ec39869efef83c64d7e
Khushbu <khushbu.thakkar@outlook.com>
2021-09-24 19:12:56 +0000

    Admin refactoring

--- routes/admin.js
+++ routes/admin.js
@@ -12,13 +12,14 @@ const { smsToOwner, smsToCustomer } = require("../lib/twilio");
 let templateVars;
 
 const adminRouter = (db) => {
+
   // GET admin dashboard
   router.get("/", (req, res) => {
     db.query(
-      "select orders.id, orders.name, orders.phone, sum(line_items.subtotal) as subTotal, orders.status, orders.order_datetime from orders join line_items on line_items.order_id = orders.id group by orders.id order by orders.id DESC;"
-    )
+      "SELECT orders.id, orders.name, orders.phone, sum(line_items.subtotal) as subTotal, orders.status, orders.order_datetime FROM orders JOIN line_items ON line_items.order_id = orders.id GROUP BY orders.id ORDER BY orders.id DESC;"
+      )
       .then((response) => {
-        //console.log(response.rows);
+
         const ordersDb = response.rows;
         const totalTime = req.body.totalTime;
         const templateVars = {
@@ -33,9 +34,9 @@ const adminRouter = (db) => {
   // GET operation order details
   router.get("/:id", (req, res) => {
     db.query(
-      `SELECT line_items.id, dishes.item, dishes.price, line_items.qty, line_items.subtotal FROM orders JOIN line_items ON line_items.order_id = orders.id JOIN dishes ON dishes.id = line_items.dish_id where orders.id = $1;`,
-      [req.params.id]
-    )
+        `SELECT line_items.id, dishes.item, dishes.price, line_items.qty, line_items.subtotal FROM orders JOIN line_items ON line_items.order_id = orders.id JOIN dishes ON dishes.id = line_items.dish_id where orders.id = $1;`,
+        [req.params.id]
+      )
       .then((response) => {
         const orderId = req.params.id;
         const anOrder = response.rows;
@@ -46,13 +47,12 @@ const adminRouter = (db) => {
       })
       .then(() => {
         return db.query(
-          "select max(estimated_time) from dishes join line_items on line_items.dish_id = dishes.id where order_id = $1;",
+          "SELECT MAX(estimated_time) FROM dishes JOIN line_items ON line_items.dish_id = dishes.id WHERE order_id = $1;",
           [req.params.id]
         );
       })
       .then((response) => {
         templateVars["maxTime"] = response.rows[0].max;
-        //  console.log("2 ", templateVars);
         res.render("admin_order_details", templateVars);
       })
       .catch((err) => console.log(err));
@@ -66,24 +66,23 @@ const adminRouter = (db) => {
       `UPDATE orders SET status = 'PROCESSED', order_datetime = CURRENT_TIMESTAMP +  ($1 * interval '1 minute') WHERE id = $2;`,
       [totalTime, req.params.id]
     )
-      .then(() => {
-        return db.query(
-          `select orders.id, orders.name, orders.phone, sum(line_items.subtotal) as subTotal, orders.status, orders.order_datetime from orders join line_items on line_items.order_id = orders.id
-       WHERE orders.id=$1
-       group by orders.id;`,[req.params.id]);
+    .then(() => {
+      return db.query(
+        `select orders.id, orders.name, orders.phone, sum(line_items.subtotal) as subTotal, orders.status, orders.order_datetime FROM orders JOIN line_items ON line_items.order_id = orders.id
+        WHERE orders.id=$1
+        GROUP BY orders.id;`,[req.params.id]);
     })
     .then((response) =>{
       const ordersDb = response.rows;
-      console.log(ordersDb[0]["order_datetime"]);
       const orderTime = ordersDb[0]["order_datetime"].toLocaleTimeString('en-CA', {timeZone: 'America/Edmonton'});
       const customerName = ordersDb[0]["name"];
       const orderId = ordersDb[0]['id'];
       smsToCustomer(orderId, customerName, orderTime);
       })
-      .then(() => {
-        res.redirect("/admin");
-      })
-      .catch((err) => console.log(err));
+    .then(() => {
+      res.redirect("/admin");
+    })
+    .catch((err) => console.log(err));
   });
   // return the router
   return router;
